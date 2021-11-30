5ec0c9d35d4312dacafa17894c84c4cf2b2fc8b8
giokszz <84277242+giokszz@users.noreply.github.com>
2021-11-30 19:20:49 +0100

    added functionality to bookhelper

--- Networking/DistLibrary/LibBookHelper/BookHelper.cs
+++ Networking/DistLibrary/LibBookHelper/BookHelper.cs
@@ -5,6 +5,7 @@ using System.Net.Sockets;
 using System.IO;
 using System.Collections.Generic;
 using LibData;
+using System.Text;
 
 namespace BookHelper
 {
@@ -31,6 +32,9 @@ namespace BookHelper
         public Message receivedMessage;
         public IPAddress IPAddress;
         public IPEndPoint localEndpoint;
+        private string bookData = @"Books.json";
+        private List<BookData> bookDataList;
+        private BookData[] books;
         public SequentialHelper()
         {
             //todo: implement the body. Add extra fields and methods to the class if needed
@@ -41,7 +45,72 @@ namespace BookHelper
 
         public void start()
         {
+            string BookContent = File.ReadAllText(bookData);
+            this.bookDataList = JsonSerializer.Deserialize<List<BookData>>(BookContent);
+            books = new BookData[this.bookDataList.Count];
+
             //todo: implement the body. Add extra fields and methods to the class if needed
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Bind(localEndpoint);
+            sock.Listen(5);
+            Socket serverSock = sock.Accept();
+            message = new Message();
+            receivedMessage = new Message();
+            messageToBeSent = new byte[1000];
+            while (true)
+            {
+                Console.WriteLine("Succesfully connected");
+
+                switch (receivedMessage.Type)
+                {
+                    case MessageType.Hello:
+                        break;
+
+                    case MessageType.Welcome:
+
+                        sock.Send(messageToBeSent);
+                        break;
+
+                    case MessageType.BookInquiry:
+                        Console.WriteLine("recieved bookInquiry");
+                        foreach (BookData d in this.bookDataList)
+                        {
+                            if (d.Title == receivedMessage.Content)
+                            {
+                                Console.WriteLine(d);
+                                message.Content = JsonSerializer.Serialize<BookData>(d);
+                                message.Type = (MessageType)4;
+                                jsonText = JsonSerializer.Serialize<Message>(message);
+                                messageToBeSent = Encoding.ASCII.GetBytes(jsonText);
+                                Console.WriteLine("book info send to server");
+                                serverSock.Send(messageToBeSent);
+                                break;
+                            }
+                        }
+                        break;
+
+                    case MessageType.UserInquiry:
+                        break;
+
+                    case MessageType.BookInquiryReply:
+                        break;
+
+                    case MessageType.UserInquiryReply:
+                        break;
+
+
+                    case MessageType.EndCommunication:
+                        break;
+
+                    case MessageType.Error:
+                        break;
+
+                    case MessageType.NotFound:
+                        break;
+                }
+
+            }
+
         }
     }
 }
